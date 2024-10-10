0afb2a289af5343141beac7a9d5c04411bfe2780
muneeb0305 <muneebahmed0305@gmail.com>
2024-10-10 18:54:39 +0500

    feat: swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Prefix
+  app.setGlobalPrefix('api');
+  // Version
+  app.enableVersioning({
+    prefix: 'v',
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('Email Parsing')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
